a1e52cb7260b304817fee0c8b31e0e95551c1ff6
Diogo Dias <DiogoDias@criticalmanufacturing.com>
2021-03-10 17:03:41 +0000

    Create first server for users using GraphQL

---
+++ users/schema/schema.js
@@ -0,0 +1,43 @@
+// Libraries
+const grapql = require('graphql');
+const axios = require('axios');
+
+const {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
+    GraphQLInt,
+} = grapql;
+
+
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString},
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(response => response.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
---
+++ users/server.js
@@ -0,0 +1,18 @@
+// Libraries
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+// Schemas
+const schema = require('./schema/schema');
+
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('Listening');
+});
