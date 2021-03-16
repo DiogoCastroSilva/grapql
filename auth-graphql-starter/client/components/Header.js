4715e0702371d5bbb813f35515f25e2eb1812ca2
Diogo Dias <DiogoDias@criticalmanufacturing.com>
2021-03-16 10:01:38 +0000

    Add logout feature

--- auth-graphql-starter/client/components/Header.js
+++ auth-graphql-starter/client/components/Header.js
@@ -2,19 +2,78 @@ import React, { Component } from 'react';
 
 // Libraries
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+
 // Queries
 import query from '../queries/currentUser';
 
+// Mutations
+import mutation from '../mutations/logout';
+
 
 class Header extends Component {
+    onLogout() {
+        this.props.mutate({
+            refetchQueries: [{
+                query
+            }]
+        });
+    }
+
+    renderButtons() {
+        const { loading, user } = this.props.data;
+        if (loading) {
+            return (
+                <div>Loading...</div>
+            );
+        }
+
+        if (user) {
+            return (
+                <li>
+                    <a onClick={this.onLogout.bind(this)}>
+                        Logout
+                    </a>
+                </li>
+            );
+        } else {
+            return (
+                <div>
+                    <li>
+                        <Link to="/signup">
+                            Signup
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/login">
+                            Login
+                        </Link>
+                    </li>
+                </div>
+            )
+        }
+
+    }
 
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <Link
+                        to="/"
+                        className="brand-logo left"
+                    >
+                        Home
+                    </Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+    graphql(query)(Header)
+);
